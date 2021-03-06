ecd460e9f0aa1b2c81bce9c63892eb54db909532
Ron.Coleman <ronncoleman@gmail.com>
2021-03-06 12:57:46 -0500

    Fixed insure when dealer up-card is an Ace.

--- src/charlie/dealer/Dealer.java
+++ src/charlie/dealer/Dealer.java
@@ -329,7 +329,7 @@ public class Dealer implements Serializable {
      * Insures against dealer Blackjack
      */
     protected void insure() {
-        throw new UnsupportedOperationException("insure is not supported");
+//        throw new UnsupportedOperationException("insure is not supported");
     }
     
     /**
